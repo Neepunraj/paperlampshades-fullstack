0b47e80ca305471986f888e5fa3f56a25f25d177
Neepun Raj Shrestha <neepushre97@gmail.com>
2025-06-29 01:09:25 +0200

    cors enabled

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 dotenv.config();
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.ORIGIN || "http://localhost:3000",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
